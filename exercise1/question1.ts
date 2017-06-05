fc0ff16e4804827d9aaa1c0e423aa5a36969b874
Yusuke Yamashita <yusuke.yamashita@livesense.co.jp>
2017-06-05 19:56:30 +0900

    question1. 型宣言

---
+++ exercise1/question1.ts
@@ -0,0 +1,5 @@
+const age: number = 32
+const birthday: string = "1985-05-01"
+const isAlive: boolean = false
+const hobby: string[] = ["fishing", "skiing"]
+const CantThinkOfGoodName: any[] = [1, true]
