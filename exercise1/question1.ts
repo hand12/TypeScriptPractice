1ad67532edd7fc410ec1e4472870ef483e28ace5
Yusuke Yamashita <yusuke.yamashita@livesense.co.jp>
2017-06-06 16:47:47 +0900

    question1. any型を削除して、配列の中身の型を個別に指定した

--- exercise1/question1.ts
+++ exercise1/question1.ts
@@ -2,4 +2,4 @@ const age: number = 32
 const birthday: string = "1985-05-01"
 const isAlive: boolean = false
 const hobby: string[] = ["fishing", "skiing"]
-const CantThinkOfGoodName: any[] = [1, true]
+const CantThinkOfGoodName: [number, boolean] = [1, true]
