04a37dfdcb6aaf28e341044beb5f9fdc3a8d621e
Yusuke Yamashita <yusuke.yamashita@livesense.co.jp>
2017-06-06 10:06:45 +0900

    question6.コンストラクタを修正した。

--- exercise1/question6.ts
+++ exercise1/question6.ts
@@ -1,12 +1,19 @@
 interface Religion {
-  name: string
-  founder: string
-  sutra: string[]
+    name: string;
+    founder: string;
+    sutra: string[];
 }
 
 class Religion implements Religion {
-    name: string
-    founder: string
-    sutra: string[]
-    constructor(name: string, founder: string, sutra: string[]) {};
+    name: string;
+    founder: string;
+    sutra: string[];
+    constructor(name: string, founder: string, sutra: string[]) {
+        this.name = name;
+        this.founder = founder;
+        this.sutra = sutra;
+    };
 }
+
+let hoge = new Religion("yusuke", "yamashita", ["hoge", "fuga"])
+alert(hoge.sutra);
