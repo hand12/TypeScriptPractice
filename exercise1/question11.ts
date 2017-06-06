b78a53679dbbb42618375e1f4a910e611e1b5c69
Yusuke Yamashita <yusuke.yamashita@livesense.co.jp>
2017-06-06 14:14:37 +0900

    question11.generic型の関数の返り値を考える

--- exercise1/question11.ts
+++ exercise1/question11.ts
@@ -1,4 +1,9 @@
-
-function duplicate<number>(arg: number): number[] {
+function duplicate<T>(arg: T): T[] {
   return [arg, arg]
 }
+ 
+ duplicate<number>(20)
+
+ generic型なので、型引数も指定する必要がある。
+ 今回はduplicate<number>と、型引数の型がnumberと指定されているので、返り値は
+ Array<number>となる。
