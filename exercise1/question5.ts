188374b1f05fd9f5e5dfaad9763f9584085b139d
Yusuke Yamashita <yusuke.yamashita@livesense.co.jp>
2017-06-07 10:34:46 +0900

    question5. TypeScriptにおけるinterfaceを調べて修正した

--- exercise1/question5.ts
+++ exercise1/question5.ts
@@ -1,5 +1,24 @@
-const giveMeYourLove = (money) => Array(money).fill("love")
-
-interface arrayLove {
-    giveMeYourLove(money: number): Array<string>;
+interface GiveMeYourLove {
+    (money: number): string[];
 }
+
+
+const giveMeYourLove: GiveMeYourLove = (money) => Array(money).fill("love")
+
+
+// メモ
+// 問題文
+// const giveMeYourLove: arrayLove = (money) => Array(money).fill("love")
+
+// type Love = "love"
+// interface GiveMeYourLove {
+//   (money: number): Love[]
+// }
+// const giveMeYourLove: GiveMeYourLove = (money) => Array(money).fill("Love")
+// type JustReturnString = (a: string) => string
+// type JustReturnNumber = (a: number) => number
+// const justReturn = (a) => a
+// // const justReturnString: JustReturnString = (a: string): string => a
+// // const justReturnNumber: JustReturnNumber = (a: number): number => a
+// const justReturnNumber = justReturn as JustReturnNumber
+// const justReturnString = justReturn as JustReturnString
